ca61daad469d016983986d6219c800e9104e31b0
Arianna <mycharacter@petrock.localdomain>
2024-03-12 09:53:54 +0100

    adds player movement and dice roll button doesnt behave

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+document.getElementById("p2").style.color = "red";
+
+var player = document.getElementById("player1");
+console.log(player);
+
+MovePlayer(1,3,1);
+console.log("hello World");
+
+function MovePlayer(x,y,p){
+    var player = document.getElementById("player1");
+    console.log(player);
+    player.style = "bottom: " + (4+y*4) + "vh; left: " + (x*4) + "%;";
+    player.style.color = "red";
+}
